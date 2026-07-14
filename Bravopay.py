e4e865fb8d9db0c9bdf7ec4a1bdce39c9fac7b23
luanrx100-ship-it <luanrx100@gmail.com>
2026-07-14 20:08:59 -0300

    Update Bravopay.py

--- Bravopay.py
+++ Bravopay.py
@@ -1,1 +1,18 @@
 pip install python-dotenv requests fastapi uvicorn qrcode pillow python-telegram-bot
+
+"""
+bravopay.py
+Integração com a API da BravoPay
+"""
+
+import requests
+from config import (
+    BRAVOPAY_BASE_URL,
+    HEADERS,
+    PRODUCT_ID
+)
+
+
+class BravoPay:
+
+    def __init__(self):
